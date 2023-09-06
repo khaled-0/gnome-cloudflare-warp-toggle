dcc2c2d8181333f5d3c7abd6838d14e0696e1791
Khaled <spkhalad@gmail.com>
2023-09-06 17:39:28 +0600

    Refactor status checking loop to be more easily readable

--- src/extension.js
+++ src/extension.js
@@ -10,29 +10,19 @@ export default class WARPToggleExtension extends Extension {
     this._indicator.quickSettingsItems.push(this._indicator._toggle);
     Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
 
-    if (
-      this._settings.get_uint("status-check-freq") > 0 &&
-      this._settings.get_boolean("status-check")
-    ) {
-      this._timeout = setInterval(
-        () => this._indicator.checkStatus(),
-        this._settings.get_uint("status-check-freq") * 1000
-      );
+    if (this._settings.get_boolean("status-check")) {
+      this.startStatusCheckLoop();
+    } else {
+      this._indicator.checkStatus();
     }
 
     this._settings.connect("changed", (settings) => {
-      if (this._timeout) clearInterval(this._timeout);
-      if (
-        settings.get_uint("status-check-freq") > 0 &&
-        settings.get_boolean("status-check")
-      )
-        this._timeout = setInterval(
-          () => this._indicator.checkStatus(),
-          settings.get_uint("status-check-freq") * 1000
-        );
+      if (settings.get_boolean("status-check")) {
+        this.startStatusCheckLoop();
+      } else {
+        if (this._timeout) clearInterval(this._timeout);
+      }
     });
-
-    this._indicator.checkStatus();
   }
 
   disable() {
@@ -46,5 +36,15 @@ export default class WARPToggleExtension extends Extension {
     }
 
     this._settings = null;
+  }
+
+  startStatusCheckLoop() {
+    if (this._timeout) clearInterval(this._timeout);
+    if (this._settings.get_uint("status-check-freq") <= 0) return;
+
+    this._timeout = setInterval(
+      () => this._indicator.checkStatus(),
+      this._settings.get_uint("status-check-freq") * 1000
+    );
   }
 }
